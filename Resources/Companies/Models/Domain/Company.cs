997e060962be1b0c943b914e3e8de92769bbcb18
DesignDocJockey <hwong18@gmail.com>
2018-11-15 23:21:43 -0500

    Edits to models

--- Resources/Companies/Models/Domain/Company.cs
+++ Resources/Companies/Models/Domain/Company.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Resources.Core.Companies
+namespace Resources.Core.Companies.Models.Domain
 {
     public class Company
     {
--- Resources/Companies/Models/View/CompanyViewModel.cs
+++ Resources/Companies/Models/View/CompanyViewModel.cs
@@ -6,5 +6,10 @@ namespace Resources.Core.Companies.Models.View
 {
     public class CompanyViewModel
     {
+        public string Name { get; set; }
+        public string CEO { get; set; }
+        public string Website { get; set; }
+        public string Symbol { get; set; }
+        public string Description { get; set; }
     }
 }
